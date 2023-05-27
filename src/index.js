e4025a409f74135a3fc42f68e75a7e44535cdbf3
SantoshChapagai <santoshchapagai100@gmail.com>
2023-05-27 13:37:43 +0300

    changes the index and app file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import AddEvent from "./pages/AddEvent";
-import BrowseEvents from "./pages/BrowseEvents";
-import EventPage from "./pages/EventPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/add" element={<AddEvent />} />
-        <Route path="/browse" element={<BrowseEvents />} />
-        <Route path="/https://www.bc.fi/" />
-        <Route path="/https://www.bc.fi/" />
-        <Route path="/events/:id" element={<EventPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+
