3a0ff25addf1455f08b42f70c909944d1b58dcee
SantoshChapagai <santoshchapagai100@gmail.com>
2023-05-27 13:18:06 +0300

    adapting the react part from Nailya

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-  const [events, setEvents] = useState([]);
-
-  useEffect(()=>{
-    axios.get('http://localhost:8004/event')
-    .then(res=>setEvents(res.data))
-    
-},[])
   return (
-    <div>
-      {events.map(event=><div key={event.id}>
-        <p>Title: {event.title}</p>
-      </div>)}
+    <div className="home-page-container">
+      <Navbar />
+      <div className="container">
+        <div className="hero-banner">
+          <h1>Welcome to BCH events</h1>
+        </div>
+        <div className="card-container">
+          <div className="card">
+            <Link to="/add">
+              <h2>Add Event</h2>
+              <p>Add a new event</p>
+            </Link>
+          </div>
+          <div className="card">
+            <Link to="/browse">
+              <h2>Browse Events</h2>
+              <p>Explore our events</p>
+            </Link>
+          </div>
+          <div className="card">
+            <Link to="https://www.bc.fi/">
+              <h2>BC's website</h2>
+              <p>Visit Our Website</p>
+            </Link>
+          </div>
+          <div className="card">
+            <Link to="https://www.myhelsinki.fi/see-and-do/events">
+              <h2>What to do in Helsinki</h2>
+              <p>Visit city of Helsinki</p>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
