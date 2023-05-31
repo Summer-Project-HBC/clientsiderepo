d2712ef582937f73e7d5eeb381df54e7aee52030
SantoshChapagai <santoshchapagai100@gmail.com>
2023-05-31 11:04:14 +0300

    fixing upcoming events in homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import './HomePage.css'
 
 const HomePage = () => {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:8004/event")
+      .then((response) => response.json())
+      .then((data) => {
+        const currentDate = new Date();
+        const upcomingEvents = data.filter(event => new Date(event.date.date) > currentDate);
+        setEvents(upcomingEvents);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching events:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="home-page-container">
       <div className="hero-banner">
@@ -16,10 +39,20 @@ const HomePage = () => {
         </div>
 
         <div className='event-container'>
-          <div className='card'>First event</div>
-          <div className='card'>First event</div>
-          <div className='card'>First event</div>
-          <div className='card'>First event</div>
+          {events.map((event, index) => (
+            <div className='card' key={index}>
+              <img className="event-photo" src={event.picture} alt={event.title} />
+              <div className='info'>
+                <p> {event.title}</p>
+                <p>Venue: {event.location}</p>
+                <p>Date: {event.date.date.slice(0, 10)}</p>
+              </div>
+              <div className='navigate'>
+                <Link to={`/browse/${event.id}`}>See more</Link>
+                <button>Register</button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
