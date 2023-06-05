798b1acac7b2580ef3cd8caff41a9cc60d5617b1
ivanpudin <ivanpudin9@gmail.com>
2023-06-05 21:31:42 +0300

    Add login feature.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
 import AddEvent from "./pages/AddEvent";
@@ -7,14 +9,68 @@ import BrowseEvents from "./pages/BrowseEvents";
 import EventPage from "./pages/EventPage";
 import FrequentQuestions from "./pages/FrequentQuestions";
 import HomePage from "./pages/HomePage";
+import Login from "./components/Login";
 
 function App() {
+  const [loginData, setLoginData] = useState({
+    logged: false,
+    username: null
+  })
+  const [user, setUser] = useState()
+  const [message, setMessage] = useState()
+  const [visibility, setVisibility] = useState(true)
+
+  const visibilityHandler = () => {
+    setVisibility(!visibility)
+    setMessage()
+  }
+
+  const onInput = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+  })
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:8004/login', user)
+      setLoginData({
+        logged:true,
+        username:response.data
+      })
+      setMessage()
+    } catch (error) {
+      setMessage(error.response.data.message)
+    }
+  }
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    setLoginData({
+      logged: false,
+      username: null
+    })
+  }
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:8004/newuser', user)
+      setMessage(response.data.message)
+    } catch (error) {
+      setMessage(error.response.data.message)
+    }
+  }
+  
+  
 
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
+        <Navbar loginData={loginData} handleLogout={handleLogout} />
+        {loginData.logged ? <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/add" element={<AddEvent />} />
           <Route path="/browse" element={<BrowseEvents />} />
@@ -22,7 +78,7 @@ function App() {
           <Route path="/https://www.bc.fi/" />
           <Route path="/https://www.bc.fi/" />
           <Route path="/browse/:individualevent" element={<EventPage />} />
-        </Routes>
+        </Routes> : <Login handleLogin={handleLogin} handleSignup={handleSignup} onInput={onInput} message={message} visibility={visibility} visibilityHandler={visibilityHandler} />}
         <Footer />
       </BrowserRouter>
     </div>
