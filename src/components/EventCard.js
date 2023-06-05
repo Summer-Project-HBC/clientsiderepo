88c3646c603fafc02e18066610ca72298f6d6002
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-05 07:00:28 +0300

    changes styling to the event

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -8,7 +8,7 @@ function EventCard({ event }) {
     <div className="event-card">
       <img className="event-photo" src={event.picture} alt={event.picture} />
       <div className="event-card-content">
-        <h2>{event.title}</h2>
+        <div className="title"><h2>{event.title}</h2></div>
         <div className="description">
           <p>Address: {event.location}</p>
           <p>Date: {event.date.date.slice(0, 10)}</p>
