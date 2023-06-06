1db0b0cd55b69687ed7b7aae6fd1fde8f6fd5c4b
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-06 15:03:51 +0300

    styling single event

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate, NavLink } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import SignUpForm from "../components/SignUpForm";
 import "./EventPage.css";
 
 function EventPage() {
   const params = useParams();
-  const navigate = useNavigate();
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
@@ -23,30 +22,22 @@ function EventPage() {
   }
 
   return (
-    <div className="event-page">
-      <div className="back-button">
-        <button className="go-back-button" onClick={() => navigate(-1)}>
-          Go back
-        </button>
-      </div>
-      <div className="event-card">
-        <div>
-
-          <img className="event-photo" src={data.picture} alt={data.title} />
-          <div className="event-card-content">
-            <h2>{data.title}</h2>
-            <div className="basic-info"> <p>{data.info}</p> </div>
-            <div className="main-info">
-              <p>Date: {data.date}</p>
-              <p>Time: {data.time}</p>
-              <p>Duration: {data.duration} hrs</p>
-              <p>Venue: {data.location} <span><NavLink to={`https://www.google.com/maps/place/${data.location}`}>↗️</NavLink></span></p>
-              <p>Transportation: {data.transport}</p>
-            </div>
-          </div>
-          <p>Sign up to recieve updates about this event</p>
-          <SignUpForm />
+    <div className="single-event-page">
+      <img className="event-photo" src={data.picture} alt={data.title} />
+      <div className="single-event-card-content">
+        <h2>{data.title}</h2>
+        <div className="basic-info"> <p>{data.info}</p> </div>
+        <div className="main-info">
+          <p>Date: {data.date?.date}</p>
+          <p>Time: {data.time?.date}</p>
+          <p>Duration: {data.duration} hrs</p>
+          <p>Venue: {data.location} <span><NavLink to={`https://www.google.com/maps/place/${data.location}`}>↗️</NavLink></span></p>
+          <p>Transportation: {data.transport}</p>
         </div>
+      </div>
+      <div className="sign-up">
+        <p>Sign up to recieve updates about this event</p>
+        <SignUpForm />
       </div>
     </div>
   );
