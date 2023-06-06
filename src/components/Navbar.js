541ffc50aa9bc323a551187c254e18466e0c42da
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-06 16:26:14 +0300

    mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar(props) {
+  const [mobileNav, setMobileNav] = useState(false);
+  const handleMobile = () => {
+    setMobileNav(!mobileNav);
+  }
   return (
     <div className="navbar">
       <div>
         <Link to="/"><h4>BCH</h4></Link>
       </div>
-      <div>
-        <nav>
-          <ul className="nav-links">
+      <button class="mobile" onClick={handleMobile}>
+        <span class="material-symbols-outlined">Menu</span>
+      </button>
+      <div >
+        <nav className={`nav-links ${mobileNav ? "mobile-nav" : ""}`}>
+          {props.loginData.logged && (
+            <p>Welcome, {props.loginData.username}</p>
+          )}
+          <ul>
             <li className="nav-item">
               <Link to="/" className="nav-link">
                 Home
@@ -31,12 +41,16 @@ function Navbar(props) {
                 FAQ?
               </Link>
             </li>
+            {props.loginData.logged && (
+              <li className="nav-item">
+                <button onClick={props.handleLogout}>Logout</button>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
-      {props.loginData.logged && <p>Welcome, {props.loginData.username}</p>}
-      {props.loginData.logged && <button onClick={props.handleLogout}>Logout</button>}
     </div>
+
   );
 }
 
